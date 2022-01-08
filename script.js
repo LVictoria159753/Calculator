f5498e08dcbe038334dbed573c6f13fafbd2d546
Victoria <Lvictoria159753@gmail.com>
2022-01-07 21:22:27 -0800

    adding files

---
+++ script.js
@@ -0,0 +1,23 @@
+let output = document.getElementById("output");
+let result = document.getElementById("result");
+let num= document.querySelectorAll("num").innerHTML;
+
+
+
+
+function Calculator{
+multiply(num1,num2){
+    return num1 * num2;}
+
+divide (num1, num2){
+return num1 / num2;}
+
+subtract(num1,num2){
+return num1 - num2;}
+
+addEventListener(num1,num2)
+{
+return num1 +  num2;
+
+}
+}
