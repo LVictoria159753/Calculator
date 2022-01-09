56f986b770fa92a3185a9ae775775653f454a095
Victoria <Lvictoria159753@gmail.com>
2022-01-09 01:33:25 -0800

    added flex containers

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
 let output = document.getElementById("output");
 let result = document.getElementById("result");
-let num= document.querySelectorAll("num").innerHTML;
+let num= document.querySelectorAll("num");
 
 
 
 
-function Calculator{
-multiply(num1,num2){
-    return num1 * num2;}
 
-divide (num1, num2){
-return num1 / num2;}
-
-subtract(num1,num2){
-return num1 - num2;}
-
-addEventListener(num1,num2)
-{
-return num1 +  num2;
-
+function multiply(num1, num2){
+    return num1 * num2;
 }
+
+function divide (num1, num2){
+    return num1 / num2;
+}
+
+function subtract(num1,num2){
+    return num1 - num2;
+}
+
+    function add (num,num){
+    return num + num;
 }
