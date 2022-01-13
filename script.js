591381b0d80d6a28414c99e995b4a92f3b11ea9c
Victoria <Lvictoria159753@gmail.com>
2022-01-13 00:33:25 -0800

    fixed display

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ let result = document.getElementById("result");
 let num= document.querySelectorAll(".num");
 let ops= document.querySelectorAll(".operator")
 
-
+/*
 //clicking a button and printing it out on the screen
 const buttons = document.querySelectorAll('button');
-/*
+
 // we use the .forEach method to iterate through each button
 buttons.forEach((button) => {
   // and for each one we add a 'click' listener
@@ -30,14 +30,14 @@ num.forEach((button) => {
       if (num1=== ''){
         num1 = e.target.innerHTML;
         num1=parseInt(num1);
-        console.log("num1:" + num1);
+            console.log("num1:" + num1); //Print first number
+            document.getElementById("upper-screen").innerHTML=num1;
       }
       else{
-   
-        num2 += e.target.innerText;
+        num2= e.target.innerText;
         num2= parseInt(num2);
-        console.log("num2:" + num2); // Print 2nd number
-
+            console.log("num2:" + num2); // Print second number
+            document.getElementById("upper-screen").innerHTML=num2;
       }
     });
 });
@@ -48,26 +48,31 @@ ops.forEach(button1 => {
     button1.addEventListener('click', (e)=> {
         if (e.target.innerHTML !== "=") {
             operator= e.target.innerHTML;
-            console.log (operator)
+                console.log (operator);
+                document.getElementById("upper-screen").innerHTML=operator;
         }
         else {
-            
+            document.getElementById("upper-screen").innerHTML=operator;
         switch (operator){  // Calculate and print output
             case "+":
                 console.log(add(num1, num2));
-                break;
+                    result= add(num1, num2);
+                        break;
   
             case "-":
                 console.log(subtract(num1, num2));
-                break;
+                    result=subtract(num1, num2);
+                        break;
             case "*":
                   console.log(multiply(num1, num2));
-                break;
+                    result=mulitiply(num1, num2);
+                        break;
             case "/":
                 console.log(divide(num1, num2));
-                break;
+                    result=divide(num1, num2);
+                        break;
         }
-
+        document.getElementById("upper-screen").innerHTML= result;
         }
     });
 });
@@ -98,16 +103,17 @@ document.querySelector("#clear").addEventListener("click", ()=>{
     }
 
 
+/*
+
+Scrap Code 
+upper-screen
+upperDisplay.textContent = evaluate(operand1, operand2, currentOperator);
+
 function operate(operator, num1, num2) {
     if (operator === "+") return add(num1, num2);
     if (operator === "-") return subtract(num1, num2);
     if (operator === "*") return multiply(num1, num2);
     if (operator === "/") return divide(num1, num2);
 }
-/*
-
-Scrap Code 
-upper-screen
-upperDisplay.textContent = evaluate(operand1, operand2, currentOperator);
 
 */
